8f415aaafe9500bd1154fc7677d30011dc594176
PengHY <53474237+PengHY-114514@users.noreply.github.com>
2023-09-26 18:44:44 +0800

    Add files via upload

---
+++ UDebug.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UDebug.h"
+#include "DrawDebugHelpers.h"
+
+
+
+UDebug::UDebug()
+{
+}
+
+UDebug::~UDebug()
+{
+}
+
+
+void UDebug::PrintToScreen(const FString Message, const double TimeToDisplay, FColor Color, const double TextScale)
+{
+	GEngine->AddOnScreenDebugMessage(-1,TimeToDisplay,Color,(TEXT("%s"), *Message),false,
+									TextScale * FVector2d::UnitVector);
+}
+
+void UDebug::PrintToLog(const FString Message)
+{
+	UE_LOG(LogTemp,Log,TEXT("%s"), *Message);
+}
+
+void UDebug::DrawSphere(APawn* Drawer, FVector Center ,const FColor Color ,bool bPersistentLines ,float LifeTime ,
+							float Radius ,int32 Segments)
+{
+	
+	DrawDebugSphere(Drawer->GetWorld(),Center,Radius,Segments,Color,bPersistentLines,LifeTime);
+}
+
+
+
+
+
---
+++ UDebug.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+/**
+ * 
+ */
+class  MYPROJECTCPP_API UDebug 
+{
+public:
+	UDebug();
+	virtual ~UDebug();
+
+	static void PrintToScreen(const FString Message, const double TimeToDisplay = 1,
+	                          FColor Color = FColor(255, 255, 255, 255),
+	                          const double TextScale = 1.36);
+
+	
+	static void PrintToLog(const FString Message);
+
+	static void DrawSphere(APawn* Drawer, FVector Center ,const FColor Color = FColor::Orange,bool bPersistentLines = false,float LifeTime = 10,
+							float Radius = 32,int32 Segments = 12);
+	
+};
